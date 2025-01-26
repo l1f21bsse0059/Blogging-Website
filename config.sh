55acc72eb67b7de71f271e61208fc6c4228cdf49
Fatima Gillani <l1f21bsse0059@ucp.edu.pk>
2025-01-26 14:49:00 +0500

    initial commit

---
+++ config.sh
@@ -0,0 +1,6 @@
+echo "Going to config the Repo"
+git branch -m develop
+git config --local user.name "Fatima Gillani"
+git config --local user.email l1f21bsse0059@ucp.edu.pk
+mkdir ./src ./styles
+touch ./src/index.html ./styles/index.css .gitignore
